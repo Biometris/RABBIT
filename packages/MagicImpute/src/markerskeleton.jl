5f2e6f7c68ad6869a00931b719c0b6ff17400af6
Zheng, Chaozhi <chaozhi@gmail.com>
2025-08-22 20:58:09 +0200

    v1.3.4

--- packages/MagicImpute/src/markerskeleton.jl
+++ packages/MagicImpute/src/markerskeleton.jl
@@ -49,7 +49,7 @@ function markerskeleton_chr(chrfhaplo::AbstractMatrix,chroffgeno::AbstractMatrix
     msg_repeatrun::AbstractString, 
     chrid::AbstractString="", 
     startit::Integer=1,
-    spacebyviterbi::Bool=false,     
+    spacebyviterbi::Bool=true,     
     verbose::Bool=true,
     io::Union{Nothing,IO}=nothing)
     (;epsfls, epsols, epsols_perind, baseerrorls,allelicbiasls,allelicoverdispersionls, allelicdropoutls) = likeerrortuple
