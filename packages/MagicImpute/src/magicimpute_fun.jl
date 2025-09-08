de1ea16025796e83ba49e4240520b67a7f66866d
Zheng, Chaozhi <chaozhi@gmail.com>
2025-09-08 10:37:42 +0200

    v1.3.4

--- packages/MagicImpute/src/magicimpute_fun.jl
+++ packages/MagicImpute/src/magicimpute_fun.jl
@@ -393,7 +393,7 @@ function magicimpute!(magicgeno::MagicGeno;
     if !isnothing(inputneighbor) && !isnothing(inputbinning)
         issubset(keys(inputbinning),keys(inputneighbor)) || @error "inconsistent inputneighbor and inputbinning"
     end
-    MagicBase.check_required_geneticmap(magicgeno; io)
+    isnothing(mapfile) && MagicBase.check_required_geneticmap(magicgeno; io)
     MagicBase.check_markerorder(magicgeno; io,verbose)    
     model = MagicBase.reset_model(magicgeno.magicped,model;io,verbose)    
     model_founderimpute = isa(model, AbstractVector) ? first(model) : model    
